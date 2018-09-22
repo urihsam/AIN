56d82b7dde9a5f421a2dc43a2f5afae66f22b54c
urihsam <mashiru.tang@gmail.com>
2018-09-21 20:59:58 -0400

    Update scripts

--- local_test_1.sh
+++ local_test_1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-python ./code/run_aan.py --local\
+python ./code/run_aan.py --local --NUM_EPOCHS 1 --BATCH_SIZE 128 --EVAL_FREQUENCY 1\
     --BETA_X 0.1\
-    --BETA_Y_LEAST 5\
-    --BETA_Y_FAKE 5\
+    --BETA_Y_LEAST 0\
+    --BETA_Y_FAKE 50\
     --BETA_Y_CLEAN 1\
-    --EPSILON 128
+    --PIXEL_BOUND 64\
+    --EPSILON 64 
--- local_train_1.sh
+++ local_train_1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 python3 ./code/run_aan.py --train --local --NUM_EPOCHS 1 --BATCH_SIZE 128 --EVAL_FREQUENCY 1\
     --BETA_X 0.1\
-    --BETA_Y_LEAST 5\
-    --BETA_Y_FAKE 5\
-    --BETA_Y_CLEAN 2\
-    --PIXEL_BOUND 16\
-    --EPSILON 128 
+    --BETA_Y_LEAST 0\
+    --BETA_Y_FAKE 50\
+    --BETA_Y_CLEAN 1\
+    --PIXEL_BOUND 64\
+    --EPSILON 64 
